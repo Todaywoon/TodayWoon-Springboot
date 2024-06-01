38b9fac14a6bc04d1bd3db701fab2d4e278afcaf
Yongha Kwon <yongha.kwon12@gmail.com>
2024-06-01 16:28:06 +0900

    [feat] init

---
+++ src/main/java/com/todaycloud/todaycloud/TodaycloudApplication.java
@@ -0,0 +1,13 @@
+package com.todaycloud.todaycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaycloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaycloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaycloud/todaycloud/TodaycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaycloud.todaycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaycloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
