d3f80292fa146213102632e2cf3722fa82a7442c
Yongha Kwon <yongha.kwon12@gmail.com>
2024-06-02 01:12:13 +0900

    [fix] file 크기 제한 추가

--- src/main/java/com/todaycloud/todaycloud/common/exception/ExceptionController.java
+++ src/main/java/com/todaycloud/todaycloud/common/exception/ExceptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 public class ExceptionController {
@@ -19,5 +20,11 @@ public class ExceptionController {
     protected ResponseEntity<ErrorCode> handleMissingHeader(MissingRequestHeaderException missingRequestHeaderException) {
 
         return ResponseEntity.badRequest().body(ErrorCode.MISMATCH_USER_INFO);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    protected ResponseEntity<ErrorCode> handleMaxFileSize(MaxUploadSizeExceededException MaxUploadSizeExceededException) {
+
+        return ResponseEntity.badRequest().body(ErrorCode.OVER_FILE_SIZE);
     }
 }
